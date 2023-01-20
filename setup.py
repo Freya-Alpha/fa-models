e56e8ad9a193eb0ad6b8c94b1648cc2abec42fed
svabra <brayan@brayan.com>
2023-01-20 16:55:29 +0100

    test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='famodels',
     packages=find_packages(include=['src'], exclude=['tests*']),
     #packages=find_packages(),
-    version='0.1.0',
+    #version='0.1.0',
     # description='The library describes the most common models used in trading systems.',
     # author='Brayan Svan',
     # license='MIT',
