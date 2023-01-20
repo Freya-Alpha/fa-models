6a0aeb0922fc34b789cf2b2f18f3028c319951df
svabra <brayan@brayan.com>
2023-01-20 11:56:57 +0100

    updated folder name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 setup(
     name='fa-models',
-    #packages=find_packages(include=['models']),
-    packages=find_packages(),
+    packages=find_packages(include=['famodels'], exclude=['tests*']),
+    #packages=find_packages(),
     # version='0.1.0',
     description='The library describes the most common models used in trading systems.',
     author='Brayan Svan',
