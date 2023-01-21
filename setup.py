c9c08152cb6f544ee8b94456aea5b68649155b83
svabra <brayan@brayan.com>
2023-01-21 10:30:28 +0100

    for development simplicity I've renamed the src forlder to famodels

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 setup(
     name='fa-models',
-    #packages=find_packages(include=['famodels'], exclude=['tests*']),
-    packages=['famodels'],
-    package_dir={'famodels':'src'}
+    packages=find_packages(include=['famodels'], exclude=['tests*']),
+    # packages=['famodels'],
+    # package_dir={'famodels':'src'}
     #packages=find_packages(),
     #version='0.1.0',
     # description='The library describes the most common models used in trading systems.',
