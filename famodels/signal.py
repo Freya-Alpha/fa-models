14909d51fa9710f049d78b02331000dbb4d0b6be
svabra <brayan@brayan.com>
2023-01-28 12:57:34 +0100

    extended signal

--- famodels/signal.py
+++ famodels/signal.py
@@ -1,7 +1,5 @@
 from typing import Optional
-
 from sqlmodel import Field, SQLModel
-
 from famodels.direction import Direction
 from famodels.side import Side
 
@@ -17,6 +15,10 @@ class BaseSignalSQLModel(SQLModel):
 class Signal(SQLModel, table=True):
     """A signal represents a signal from a algorithm and supplier. It must have a correlating id to a trade."""
     id: Optional[int] = Field(default=None, primary_key=True)
+    algo_id: str
+    """Provide the id of your algorithm entity (you might have more than one algorithm), which is sending this signal.  We have issued this id."""
+    provider_id: str
+    """The ID of the provider, who emitted the signal."""
     market: str    
     exchange: str
     trade_correlation_id: str
@@ -27,4 +29,11 @@ class Signal(SQLModel, table=True):
     price: float
     tp: float
     sl: float
-    # price: Optional[int] = None
+    datetime_of_creation: Optional[str]
+    """An ISO-UTC-compatible date and time of when the signal was created by the signal supplier."""
+    datetime_of_registration: Optional[str]
+    """The ISO-UTC-compatible date and time of when the signal was entering our interface."""
+    position_size_of_investement: float = 100
+    """Percentage of the investment position this algortihm is allowed to trade. Default is 100%, which is 1 position."""  
+    
+
