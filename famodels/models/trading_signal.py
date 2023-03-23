7963b4d6d8ad69ca30d2eb3d97a67619ee0d9f99
svabra <brayan@brayan.com>
2023-03-23 09:09:11 +0100

    typos

--- famodels/models/trading_signal.py
+++ famodels/models/trading_signal.py
@@ -22,10 +22,16 @@ class TradingSignal(SQLModel, table=True):
     """The id is marked optional - because it will be created by either SQL DB or a async method in an event-driven system.
         If you add an id here, it will be copied to the trade_correlation_id.
     """
+    supplier_correlation_id: Optional[str]
+    """You can use this correlation id as your own 'signal id' of your internal system. 
+    Do not mistaken this correlation id with the trade correlation id."""    
     trade_correlation_id: str
     """FA Models describes a Trade as a buy and a sell (not soley a buy or a sell). 
     Every trade is expected to consist of at least one buy order and zero or more sell orders. 
-    Thus, the trade_correlation_id is mandatory. Use this correlation id link your signals to a trade."""
+    Thus, the trade_correlation_id is mandatory. Use this correlation id to link your signals to a trade. In other words, it is a grouping functionality."""
+    is_hot_signal: bool = False
+    """By default, every signal is marked as a cold signal. Thus, set to false. That is a paper-trading signal and will only be processed for forward-performance testing. 
+    Hot signals are suggested to be processed by the order engines - provided all other requirements for hot trading are fulfilled."""    
     algo_id: str
     """Provide the id of your algorithm id (you might have more than one algorithm), which is sending a signal. """
     provider_id: str
