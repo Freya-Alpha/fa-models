eb89b7f8773910359eef77e5f0a16ee85ed95679
svabra <brayan@brayan.com>
2023-04-04 10:32:23 +0200

    processed signals needs to use the kwargs

--- famodels/models/processed_trading_signal.py
+++ famodels/models/processed_trading_signal.py
@@ -1,31 +1,19 @@
-from ast import List
+from typing import List
 from famodels.models.trading_signal import TradingSignal
 from famodels.models.state_of_signal import StateOfSignal
 
 class ProcessedTradingSignal(TradingSignal):
-    """As soon a Signal is processed by the signal qualifier, it is declared as a Processed Signal."""
+    """As soon a trading signal is processed by the signal qualifier, it is declared as a Processed Signal.
+        When instantiating, use the **kwargs to map the TradingSignal values to the attributes of the processed trading signal.
+        e.g. ProcessedTradingSignal(status=StateOfSignal.ACCEPTED, process_info=[], **signal.__dict__)
+    """
+    status: StateOfSignal
+    process_info: List[str]
 
-    def __init__(self, signal: TradingSignal, status: StateOfSignal, process_info: List = None):
-        self.status: StateOfSignal = status
-        super().__init__(
-            id=signal.id,
-            provider_signal_id=signal.provider_signal_id,
-            provider_trade_id=signal.provider_trade_id,
-            is_hot_signal=signal.is_hot_signal,
-            algo_id=signal.algo_id,
-            provider_id=signal.provider_id,
-            market=signal.market,
-            exchange=signal.exchange,
-            direction=signal.direction,
-            side=signal.side,
-            price=signal.price,
-            tp=signal.tp,
-            sl=signal.sl,
-            timestamp_of_creation=signal.timestamp_of_creation,
-            timestamp_of_registration=signal.timestamp_of_registration,
-            position_size_of_investment=signal.position_size_of_investment
-        )        
-        self.process_info: List[str] = process_info
+    class Config:
+        orm_mode = True
+
+
         
 
     
---
+++ tests/test_processed_trading_signal.py
@@ -0,0 +1,21 @@
+
+from time import time
+import pytest
+from famodels.models.direction import Direction
+from famodels.models.processed_trading_signal import ProcessedTradingSignal
+from famodels.models.side import Side
+from famodels.models.state_of_signal import StateOfSignal
+
+from famodels.models.trading_signal import TradingSignal
+
+SIGNAL_1 = TradingSignal(id="10", provider_signal_id="sifwi", provider_trade_id="wuwor232", is_hot_signal=False, algo_id="232424", market="BTC/USDT", exchange="binance", direction=Direction.LONG, side=Side.BUY, price=20000, tp=22000, sl=19000, timestamp_of_creation=int(time()*1000) )
+SIGNAL_2 = TradingSignal(id="10", provider_signal_id="sifwi", provider_trade_id="wuwor232", is_hot_signal=False, algo_id="232424", market="BTC/USDT", exchange="binance", direction=Direction.LONG, side=Side.BUY, price=20000, tp=22000, sl=19000, timestamp_of_creation=int(time()*1000) )
+
+@pytest.mark.parametrize("signal, invalidations, expectation", [
+    (SIGNAL_1, ["Failed to do something smart.", "And also this"] ,True),
+    (SIGNAL_2, [], True)
+])
+def test_instantiation(signal:TradingSignal, invalidations, expectation):
+    proc_signal = ProcessedTradingSignal(status=StateOfSignal.ACCEPTED, process_info=[], **signal.__dict__)
+    assert proc_signal.id == "10"
+    assert proc_signal.status == StateOfSignal.ACCEPTED
