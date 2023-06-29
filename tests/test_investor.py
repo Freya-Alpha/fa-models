cd0fdef78a1d6c280688ab4a684e0985406805c9
svabra <brayan@brayan.com>
2023-06-29 10:09:29 +0200

    add the bind 0..0.0. to avoid the redis protected-mode.

--- tests/test_investor.py
+++ tests/test_investor.py
@@ -15,20 +15,8 @@ def setup_redis():
         os.system('docker stop redis-unit-test')
         os.system('docker rm redis-unit-test')
 
-# @pytest.fixture(scope="function", autouse=True)
-# def setup_redis():
-#     os.system('docker run -d --name redis-unit-test -p 6379:6379 -p 8001:8001 redis/redis-stack:latest ')
-#     # give some time for the Redis server to start
-#     time.sleep(2)
-#     yield
-#     os.system('docker stop redis-unit-test')
-#     os.system('docker rm redis-unit-test')
 
 def test_investor_model():
     # Now we can create an instance of `Investor` without connecting to a real Redis server
     investor = Investor(investor_id='123', email='test@example.com')
-
-    # # The `passphrase` property raises an exception, as expected
-    # with pytest.raises(Exception):
-    #     pass
 
